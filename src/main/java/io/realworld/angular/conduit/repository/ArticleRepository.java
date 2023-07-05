eec98986f9053c2031dc5f5e065894aeca240082
ayubxon0880 <AyubxonNT@gmail.com>
2023-07-05 19:49:52 +0500

    Modellar uchun repositorylar yaratildi

--- src/main/java/io/realworld/angular/conduit/repository/ArticleRepository.java
+++ src/main/java/io/realworld/angular/conduit/repository/ArticleRepository.java
@@ -4,7 +4,6 @@ import io.realworld.angular.conduit.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface ArticleRepository extends JpaRepository<Article,Long> {
 }
