d9e950ef7634d4a6b097cd50a7057684a1065363
ayubxon0880 <AyubxonNT@gmail.com>
2023-07-05 19:34:06 +0500

    Add Models

---
+++ src/main/java/io/realworld/angular/conduit/model/Likes.java
@@ -0,0 +1,26 @@
+package io.realworld.angular.conduit.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Likes {
+    @Id
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private Article article;
+}
