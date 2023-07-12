7539e1f05e3647addcc91d7c21118044ee8af684
ayubxon0880 <AyubxonNT@gmail.com>
2023-07-12 17:21:25 +0500

    ArticleServiceImpl ga o'zgarishlar kiritildi, Security qismi boshlandi

--- src/main/java/io/realworld/angular/conduit/service/impl/TagServiceImpl.java
+++ src/main/java/io/realworld/angular/conduit/service/impl/TagServiceImpl.java
@@ -2,17 +2,28 @@ package io.realworld.angular.conduit.service.impl;
 
 import io.realworld.angular.conduit.dto.CommonResponse;
 import io.realworld.angular.conduit.dto.TagDTO;
+import io.realworld.angular.conduit.mapper.TagMapper;
+import io.realworld.angular.conduit.model.Tag;
+import io.realworld.angular.conduit.repository.TagRepository;
 import io.realworld.angular.conduit.service.TagService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class TagServiceImpl implements TagService {
+    private final TagRepository tagRepository;
 
     @Override
     public ResponseEntity<CommonResponse<List<TagDTO>>> getAll() {
-        return null;
+        List<TagDTO> list = tagRepository.findAll().stream().map(TagMapper::toDto).toList();
+        CommonResponse<List<TagDTO>> commonResponse = new CommonResponse<>();
+        commonResponse.add("tags",list);
+
+        return ResponseEntity.ok(commonResponse);
+
     }
 }
